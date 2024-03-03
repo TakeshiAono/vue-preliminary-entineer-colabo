55bc0b62e0281acd8e2f90ed759b38e7afda292a
TakeshiAono <takeshi.aono001@gmail.com>
2024-03-03 18:25:32 +0900

    Loginページの実装

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import MyPageView from '@/views/MyPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/account',
+      name: 'account',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: MyPageView
     }
   ]
 })
---
+++ src/views/MyPageView.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import router from '@/router';
+import axios from 'axios';
+import { ref } from 'vue';
+import { RouterView } from 'vue-router';
+
+// type LoginModel = {
+//   email: string | null
+//   password: string | null
+// }
+
+const API_URL = import.meta.env.VITE_API_SERVER_URI;
+// const loginModel = ref<LoginModel>({
+//   email: null,
+//   password: null,
+// })
+
+// const a = ref(true)
+// const aa = () => {console.log("aser")}
+// console.log(API_URL)
+// const submitHandler = (event): void => {
+//   axios.post(`${API_URL}/login`, { "password": loginModel.value.password, "email": loginModel.value.email })
+//     .then((data) => {
+//       console.log(data, "成功")
+//       router.push("account")
+//     })
+//     .catch((error) => {
+//       console.log(error, "エラー")
+//       router.push("mypage")
+//     })
+// }
+</script>
+
+<template>
+  <main>
+    <!-- <n-form ref="formRef" :model="loginModel">
+      <n-form-item path="email" label="Email">
+        <n-input v-model:value="loginModel.email" @keydown.enter.prevent />
+      </n-form-item>
+      <n-form-item path="password" label="Password">
+        <n-input v-model:value="loginModel.password" type="password" @keydown.enter.prevent />
+      </n-form-item>
+      <div style="display: flex; justify-content: flex-end">
+        <n-button :disabled="loginModel.email === null && loginModel.password === null" round type="primary" @click="submitHandler">
+          Validate
+        </n-button>
+      </div>
+    </n-form> -->
+    <p>mypage</p>
+  </main>
+</template>
+
+<style scoped>
+p {
+  background-color: red;
+}
+
+header {
+  background-color: blue;
+}
+
+footer {
+  background-color: yellow;
+}
+</style>
