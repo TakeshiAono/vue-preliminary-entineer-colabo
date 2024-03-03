b88af1b23daa79ecd713f91844499693c5c29215
TakeshiAono <takeshi.aono001@gmail.com>
2024-03-03 22:15:45 +0900

    文言修正

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,12 +17,11 @@ const loginModel = ref<LoginModel>({
 const submitHandler = (): void => {
   axios.post(`${API_URL}/login`, { "password": loginModel.value.password, "email": loginModel.value.email })
     .then((data) => {
-      console.log(data, "成功")
+      console.log(data)
       router.push("myPage")
     })
     .catch((error) => {
-      console.log(error, "エラー")
-      router.push("account")
+      console.log(error)
     })
 }
 </script>
@@ -30,7 +29,7 @@ const submitHandler = (): void => {
 <template>
   <main>
     <div id="login-container">
-      <h2 id="login-title">ログイン</h2>
+      <h2 id="login-title">Login</h2>
       <n-form ref="formRef" :model="loginModel" id="login-form">
         <n-form-item path="email" label="Email">
           <n-input v-model:value="loginModel.email" @keydown.enter.prevent />
@@ -41,7 +40,7 @@ const submitHandler = (): void => {
         <div style="display: flex; justify-content: flex-end">
           <n-button :disabled="(loginModel.email == null || loginModel.password == null)" round type="primary"
             @click="submitHandler">
-            登録
+            Login
           </n-button>
         </div>
       </n-form>
