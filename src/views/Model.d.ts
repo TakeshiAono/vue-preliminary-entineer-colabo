528c7a729c7dce3e7b61788a8ac365ed675ef25d
TakeshiAono <takeshi.aono001@gmail.com>
2024-04-14 16:05:09 +0900

    定義追加

--- src/views/Model.d.ts
+++ src/views/Model.d.ts
@@ -1,13 +1,25 @@
 type Project = {
-id: string;
-name: string;
-iconUrl: string;
-description: string;
-ownerIds: string[];
-membersIds: string[];
-projectNoticeIds: string[];
-directoryIds: string[];
-fileIds: string[];
-chatRoomIds: string[];
-userIds: string[];
+  id: string;
+  name: string;
+  iconUrl: string;
+  description: string;
+  ownerIds: string[];
+  membersIds: string[];
+  projectNoticeIds: string[];
+  directoryIds: string[];
+  fileIds: string[];
+  chatRoomIds: string[];
+  userIds: string[];
+}
+
+type Task = {
+  id: string;
+  name: string;
+  deadline: Date;
+  isDone: boolean;
+  projectId: string;
+  inChargeUserId: string;
+  description: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
