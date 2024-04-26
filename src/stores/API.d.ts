228e9beffd2a01d4d9b4a485ee96eb1f5abb55d0
TakeshiAono <takeshi.aono001@gmail.com>
2024-04-27 08:28:40 +0900

    型ファイル作成

--- src/stores/API.d.ts
+++ src/stores/API.d.ts
@@ -1,18 +1,98 @@
 // / <reference types="vite/client" />
-type GetResponseUser =
-{
-  id: string,
-  name: string,
-  email: string,
-  password: string,
-  iconUrl: string,
-  introduce: string,
-  followerIds: string[],
-  skillIds: string[],
-  rollIds: string[],
-  channelIds: string[],
-  messageIds: string[],
-  offerIds: string[],
-  userNoticeIds: string[],
-  projectIds: string[],
+type ResponseUser = {
+  id: number | null
+  name: string | null
+  email: string
+  password: string
+  iconUrl: string | null
+  introduce: string | null
+  isOwner: boolean | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  userNotices: UserNotice[]
+  messages: Message[]
+  skills: Skill[]
+  roles: Role[]
+  channels: Channel[]
+  offers: Offer[]
+  tasks: Task[]
+  scoutedOffers: Offer[]
+  followers: Follower[]
+  projects: Project[]
+}
+
+type ResponseChannel = {
+  id: number | null
+  name: string
+  userId: number | null
+  chatRoomId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  messages: Message[]
+}
+
+type ResponseChatRoom = {
+  id: number | null
+  name: string
+  projectId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  channels: Channel[]
+}
+
+type ResponseDirectory = {
+  id: number | null
+  name: string
+  projectId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  files: File[]
+}
+
+type ResponseFile = {
+  id: number | null
+  name: string
+  fileUrl: string
+  directoryId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
+}
+
+type ResponseMessage = {
+  id: number | null
+  text: string
+  content: string
+  userId: number | null
+  channelId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
+}
+
+type ResponseProject = {
+  id: number | null
+  name: string
+  iconUrl: string | null
+  description: string | null
+  deadline: Date | null
+  createdAt: Date | null
+  updatedAt: Date | null
+  owners: Owner[]
+  projectNotices: ProjectNotice[]
+  directories: Directory[]
+  chatRooms: ChatRoom[]
+  operations: Operation[]
+  tasks: Task[]
+  users: User[]
+}
+
+type ResponseTask = {
+  id: number | null
+  name: string
+  doneAt: Date | null
+  deadline: Date | null
+  description: string | null
+  projectId: number | null
+  inChargeUserId: number | null
+  createdAt: Date | null
+  updatedAt: Date | null
 }
