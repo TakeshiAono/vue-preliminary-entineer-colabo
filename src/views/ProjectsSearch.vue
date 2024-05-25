1d4d5357b50a4c85ca6aeb4cdc35668ebe4e4e10
TakeshiAono <takeshi.aono001@gmail.com>
2024-05-25 18:14:15 +0900

    プロジェクト一覧取得機能の実装

--- src/views/ProjectsSearch.vue
+++ src/views/ProjectsSearch.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
-import router from '@/router';
 import { useUserStore } from '@/stores/userStore';
 import axios from 'axios';
-import { computed, ref } from 'vue';
-import { RouterView } from 'vue-router';
+import { onMounted, ref, watch } from 'vue';
 
 const userStore = useUserStore()
+const projects = ref([])
+
+onMounted(async () => {
+  projects.value = await fetchProjects()
+})
+
+async function fetchProjects() {
+  const API_URL = import.meta.env.VITE_API_SERVER_URI
+  const response = await axios.get(`${API_URL}/projects`)
+  return response.data
+}
 </script>
 
 <template>
   <main>
+    <p>{{ projects.map(project => project.name) }}</p>
     <h1>ProjectsSearchPage</h1>
     <p>ログイン中: {{ userStore.isLogin }}</p>
   </main>
