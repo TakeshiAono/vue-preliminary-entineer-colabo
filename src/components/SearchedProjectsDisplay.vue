bb339077f67f113c4f219dcfc6440618f7ca3c7f
TakeshiAono <takeshi.aono001@gmail.com>
2024-06-01 00:58:43 +0900

    ownerId関連の実装(アイコン以外)

--- src/components/SearchedProjectsDisplay.vue
+++ src/components/SearchedProjectsDisplay.vue
@@ -1,20 +1,47 @@
 <script setup lang="ts">
+import axios from 'axios';
 import _ from 'lodash';
+import { ref, watch } from 'vue';
 
 type propsType = {
   searchedProjects?: Project[];
 }
 
 const props: propsType = defineProps(["searchedProjects"])
+const projectUserMaps = ref<{projectId: number, user: User}[] | []>([])
+
+watch(props, async () => {
+  if (props.searchedProjects) {
+    projectUserMaps.value = await Promise.all(props.searchedProjects.map(async (project) => {
+      const owner = await fetchUser(project.ownerId)
+      return {projectId: project.id, user: owner}
+    }))
+  }
+})
+
+async function fetchUser(id: number) {
+  const API_URL = import.meta.env.VITE_API_SERVER_URI
+  const response = await axios.get(`${API_URL}/users/${id}`)
+  return response.data
+}
+
+const getProjectUser = (projectId: number) => {
+  const projectUserMap = projectUserMaps.value.find(projectUserMap => projectUserMap.projectId === projectId)
+  return projectUserMap && projectUserMap.user
+}
 
 </script>
 
 <template>
-  <div v-for="project in props.searchedProjects">
+  <div :key="project.id" v-for="project in props.searchedProjects">
     <div class="projectCard">
       <div class="cardHeader">
         <h1>{{ project.name }}</h1>
-        <p>{{ project.ownerIds }}</p>
+        <div>
+          <span>責任者: </span>
+          <span>{{ getProjectUser(project.id)?.iconUrl }}</span>
+          <span>{{ getProjectUser(project.id)?.name }}</span>
+        </div>
       </div>
       <div class="projectContent">
         <div>
@@ -26,7 +53,7 @@ const props: propsType = defineProps(["searchedProjects"])
         <div>
           <h2>関連技術</h2>
           <div class="technologyContent">
-            <div v-for="technologyText in _.map(project.useTechnology, (value, key) => `${key + ': ' + value}`)">
+            <div :key="project.id + technologyText" v-for="technologyText in _.map(project.useTechnology, (value, key) => `${key + ': ' + value}`)">
               <p>{{ technologyText }}</p>
             </div>
           </div>
@@ -34,7 +61,7 @@ const props: propsType = defineProps(["searchedProjects"])
         <div>
           <h2>募集メンバー</h2>
           <div class="recruitingMemberJobContent">
-            <div v-for="recruitingMemberJobText in _.map(project.recruitingMemberJob, (value, key) => `${key + ': ' + value + '人'}`)">
+            <div :key="project.id + recruitingMemberJobText" v-for="recruitingMemberJobText in _.map(project.recruitingMemberJob, (value, key) => `${key + ': ' + value + '人'}`)">
               <p>{{ recruitingMemberJobText }}</p>
             </div>
           </div>
@@ -78,7 +105,8 @@ h2 {
 
 .cardHeader {
   display: flex;
-  justify-content: space-between;
+  justify-content:space-between;
+  align-items: center;
 }
 
 .projectContent {
