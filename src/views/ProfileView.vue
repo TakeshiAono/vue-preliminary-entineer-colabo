0dbc20ea542e428f0cad8f90751122d458fbdfba
ayumi nakano <aynminoa@gmail.com>
2024-07-24 21:15:20 +0900

    offerを出す実装

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,24 +1,39 @@
 <template>
   <main>
-    <p>ログイン中のユーザー{{ userStore.currentUser.name }}</p>
-    <h1>{{ user?.name }}さんのプロフィール</h1>
+    <n-modal
+      v-model:show="showModal"
+      :mask-closable="false"
+      preset="dialog"
+      negative-text="キャンセル"
+      @negative-click="onNegativeClick"
+      class="custom-modal"
+    >
+    <SubmitOffer :scoutedUserId="user?.id" />
+    </n-modal>
+    <!-- <p>ログイン中のユーザー{{ userStore.currentUser.name }}</p> -->
+    <div id="selected-user-name">
+      <h1>{{ user?.name }}さんのプロフィール</h1>
+      <n-button @click="showModal = true">
+        オファーを出す
+      </n-button>
+    </div>
     <UserIntroduction :user="user" />
     <UserProjects :projectIds="user?.projectIds || []" />
     <UserFollowers :userId="user?.id" /> 
-    <SubmitOffer :scoutedUserId="user?.id" />
   </main>
 </template>
 
 <script setup lang="ts">
 import SubmitOffer from "@/components/SubmitOffer.vue";
 import UserFollowers from "@/components/UserFollowers.vue";
-import UserIntroduction from "@/components/UserIntroduction.vue";
 import UserProjects from "@/components/UserProjects.vue";
 import { useProjectStore } from '@/stores/projectStore';
 import { useUserStore } from "@/stores/userStore";
 import { User } from "@/types";
 import { onMounted, ref } from "vue";
 import { useRoute } from 'vue-router';
+
+const showModal = ref(false)
 
 const userStore = useUserStore();
 const user = ref<User | null>(null);
@@ -41,3 +56,27 @@ onMounted(async () => {
 });
 
 </script>
+
+<style scoped>
+#selected-user-name{
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+</style>
+
+<style>
+.n-dialog__title .n-dialog__icon {
+  display: none;
+}
+.n-dialog__content{
+  margin-bottom: 8px !important;
+  
+}
+.custom-modal {
+  width: 600px !important; 
+  height: 600px !important; 
+}
+
+</style>
+
