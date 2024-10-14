354487b63a7921e1ecdb5246db55a143b42e889f
aynminoa <aynminoa@gmail.com>
2024-10-14 20:42:00 +0900

    メンバー表示の実装

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ProjectDescription from "@/components/ProjectDescription.vue"
+
 import { useProjectStore } from "@/stores/projectStore"
 import { useUserStore } from "@/stores/userStore"
 import { onMounted, ref } from "vue"
@@ -7,34 +8,42 @@ import { useRoute } from "vue-router"
 
 const userStore = useUserStore()
 const projectStore = useProjectStore()
-const project = ref(null)
+const project = ref({ name: "", description: "" })
+const projectUsers = ref([])
 const route = useRoute()
 const projectId = parseInt(route.params.id as string, 10)
 
 onMounted(async () => {
   project.value = await projectStore.fetchProject(projectId)
+  if (project.value?.userIds) {
+    const users = await Promise.all(
+      project.value.userIds.map(async (userId: number) => await userStore.getUserInfo(userId)),
+    )
+    projectUsers.value = users
+  }
 })
 </script>
 
 <template>
   <main>
-    <h1>ProjectShow</h1>
+    <h1>{{ project.name }}</h1>
     <div v-if="project">
       <ProjectDescription :description="project.description" />
+      <h1 id="project-member-title">メンバー</h1>
+      <div id="project-member-content">
+        <div v-if="projectUsers.length">
+          <p v-for="user in projectUsers" :key="user.id">{{ user.name }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped>
-p {
-  background-color: red;
-}
-
-header {
-  background-color: blue;
-}
-
-footer {
-  background-color: yellow;
+#project-member-content {
+  width: 200px;
+  height: 100px;
+  border-radius: 10px;
+  border: solid;
 }
 </style>
