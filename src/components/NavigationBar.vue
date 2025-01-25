317f5c9c1fc821cf9e1e7f5d73d6cadeaf1c9e3c
TakeshiAono <takeshi.aono001@gmail.com>
2025-01-25 11:33:27 +0900

    フォーマット適用

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -13,7 +13,9 @@ const userStore = useUserStore()
           <RouterLink to="/tasks" class="nav-content">タスク一覧</RouterLink>
           <RouterLink to="/myPage" class="nav-content">MyPage</RouterLink>
           <!-- 仮でパスパラメータにid1を入れている。 -->
-          <RouterLink :to="`/users/${userStore.currentUser.id}`" class="nav-content">Profile</RouterLink>
+          <RouterLink :to="`/users/${userStore.currentUser.id}`" class="nav-content"
+            >Profile</RouterLink
+          >
           <RouterLink to="/projects/search" class="nav-content">projectsSearch</RouterLink>
           <RouterLink
             to="/login"
